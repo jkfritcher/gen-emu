fbd626c7b970d8977814e2c59a711829a9fb3b4b
Jason Fritcher <jkf@wolfnet.org>
2002-05-01 17:06:25 +0000

    Added volatile typedefs.

--- gen-emu.h
+++ gen-emu.h
@@ -10,6 +10,14 @@ typedef unsigned char	uint8_t;
 typedef unsigned short	uint16_t;
 typedef unsigned int	uint32_t;
 
+typedef volatile signed char	vsint8_t;
+typedef volatile signed short	vsint16_t;
+typedef volatile signed int		vsint32_t;
+
+typedef volatile unsigned char	vuint8_t;
+typedef volatile unsigned short	vuint16_t;
+typedef volatile unsigned int	vuint32_t;
+
 extern uint8_t debug;
 
 #endif /* _GEN_EMU_H_ */
