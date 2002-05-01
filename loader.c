7fb1415e6728211f5dac0de26301ce11a4ea99f9
Jason Fritcher <jkf@wolfnet.org>
2002-05-01 16:50:27 +0000

    Rearranged some code, changed a few messages and commented out the write-back of the rom to the pc. With the exception of the debug code, the loader should be stable.

--- loader.c
+++ loader.c
@@ -9,10 +9,12 @@
 
 uint8_t *rom_load(char *name)
 {
-	uint8_t *rom;
-	uint32_t fd, len, cksum, i;
-	MD5_CTX ctx;
+	uint32_t fd, len, i;
+	uint8_t *rom = NULL;
 	uint8 md5sum[16];
+	MD5_CTX ctx;
+
+	printf("Loading rom %s ... ", name);
 
 	fd = fs_open(name, O_RDONLY);
 	if (fd == 0) {
@@ -23,12 +25,10 @@ uint8_t *rom_load(char *name)
 	len = fs_seek(fd, 0, SEEK_END);
 	fs_seek(fd, 0, SEEK_SET);
 
-	if (debug)
-		rom = malloc(4 * 1024 * 1024);
+	rom = malloc(4 * 1024 * 1024);
 
 	if (strstr(name, ".bin") != NULL) {
-		if (!debug)
-			rom = malloc(len);
+//		rom = malloc(len);
 		if (rom == NULL) {
 			printf("rom_load(): malloc() failed.\n");
 			goto error;
@@ -40,8 +40,7 @@ uint8_t *rom_load(char *name)
 		uint8_t buf[16384];
 
 		len -= 512;
-		if (!debug)
-			rom = malloc(len);
+//		rom = malloc(len);
 		if (rom == NULL) {
 			printf("rom_load(): malloc() failed.\n");
 			goto error;
@@ -63,6 +62,9 @@ uint8_t *rom_load(char *name)
 					*tmp++ = buf[j];
 				}
 			}
+		} else {
+			printf("rom_load(): Invalid .smd file loaded.\n");
+			goto error;
 		}
 	} else {
 		printf("ROM is not a recognized format.\n");
@@ -71,15 +73,17 @@ uint8_t *rom_load(char *name)
 
 	fs_close(fd);
 
-	fd = fs_open("/pc/home/jkf/src/dc/gen-emu/roms/rom.bin", O_WRONLY);
-	fs_write(fd, rom, len);
-	fs_close(fd);
+	printf("Done.\n");
+
+//	fd = fs_open("/pc/home/jkf/src/dc/gen-emu/roms/rom.bin", O_WRONLY | O_TRUNC);
+//	fs_write(fd, rom, len);
+//	fs_close(fd);
 
 	MD5Init(&ctx);
 	MD5Update(&ctx, rom, len);
 	MD5Final(md5sum, &ctx);
 
-	printf("ROM MD5 checksum = ");
+	printf("ROM MD5 = ");
 	for(i = 0; i < 16; i++)
 		printf("%02x", md5sum[i]);
 	printf("\n");
