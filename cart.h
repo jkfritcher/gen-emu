05269251535c00b6b2e34376df69805dfda1a82c
Jason Fritcher <jkf@wolfnet.org>
2002-05-16 19:39:33 +0000

    Cartridge struct to hold rom/save ram info.

---
+++ cart.h
@@ -0,0 +1,19 @@
+/* $Id$ */
+
+#ifndef _CART_H_
+#define _CART_H_
+
+typedef struct cart_s {
+	uint8		*rom;
+	uint32_t	rom_len;
+	uint8		*sram;
+	uint32_t	sram_len;
+	uint32_t	sram_start;
+	uint32_t	sram_end;
+	uint32_t	banks[8];
+	uint8_t		is_ssf2;
+} cart_t;
+
+extern cart_t cart;
+
+#endif /* _CART_H_ */
