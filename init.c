6d59d5dba950d909b80b90d94544615294a0bdd3
Jason Fritcher <jkf@wolfnet.org>
2002-08-29 19:44:05 +0000

    misc changes... comment out sound code, change border color, reformat the pvr_param struct.

--- init.c
+++ init.c
@@ -10,17 +10,17 @@
 
 
 pvr_init_params_t pvr_params = {
-	{ PVR_BINSIZE_16, PVR_BINSIZE_0, PVR_BINSIZE_0, PVR_BINSIZE_0, PVR_BINSIZE_0 },
-	256 * 1024
+	{ PVR_BINSIZE_16, PVR_BINSIZE_0, PVR_BINSIZE_0,
+	  PVR_BINSIZE_0, PVR_BINSIZE_0 }, 256 * 1024
 };
 
-extern SN76489 PSG; 
+//extern SN76489 PSG; 
 
 
 void gen_init(void)
 {
 	pvr_init(&pvr_params);
-	vid_border_color(0, 0, 0);
+	vid_border_color(0, 0, 255);
 	pvr_set_bg_color(0, 0, 1.0f);
 
 	vdp_init();
@@ -32,8 +32,8 @@ void gen_reset(void)
 	m68k_pulse_reset();
 	z80_init();
 
-	Reset76489(&PSG, 0);
-	Sync76489(&PSG, SN76489_SYNC);
+//	Reset76489(&PSG, 0);
+//	Sync76489(&PSG, SN76489_SYNC);
 
 	ctlr_reset();
 }
